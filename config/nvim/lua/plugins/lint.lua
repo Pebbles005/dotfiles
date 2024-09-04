89412f55720c58c3bab5f5d077afe508b5792eba
Pebbles005 <pebbles74788@gmail.com>
2024-09-04 16:22:22 +0530

    updated lint

--- config/nvim/lua/plugins/lint.lua
+++ config/nvim/lua/plugins/lint.lua
@@ -51,7 +51,7 @@ return {
 			format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
 			formatters_by_ft = {
 				lua = { "stylua" },
-				svelte = { "prettier" },
+				-- svelte = { "prettier" },
 				javascript = { "prettier" },
 				typescript = { "prettier" },
 				javascriptreact = { "prettier" },
