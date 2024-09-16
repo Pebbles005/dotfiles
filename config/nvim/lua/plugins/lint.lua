d7bff2e832d859b8894e3177ddb47e0b938ff5fb
Pebbles005 <pebbles74788@gmail.com>
2024-09-16 15:15:39 +0530

    added iso preview

--- config/nvim/lua/plugins/lint.lua
+++ config/nvim/lua/plugins/lint.lua
@@ -48,7 +48,7 @@ return {
 			"BufNewFile",
 		},
 		opts = {
-			format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+			format_on_save = { lsp_format = "fallback" },
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- svelte = { "prettier" },
